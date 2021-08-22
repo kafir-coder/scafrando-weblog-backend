9689735e6754f4808292f8d994d5bb8d1ca0e140
Caio <caiotony16@gmail.com>
2021-08-22 01:55:43 +0100

    fix(server): resolving eslint and prettier conflit with tabs spaces times

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   'plugins': ['@typescript-eslint'],
   'rules': {
-    'indent': ['error', 4],
+    'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
